989f09849ac787eb79504b757a117593bcc29d9f
joieng1 <johncieng1@gmail.com>
2024-04-04 16:53:50 -0700

    implemented Table componenet with delete function

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,11 +1,42 @@
 // src/MyApp.jsx
 import React from "react";
+import Table from "./Table";
 
 function MyApp() {
+  const [characters, setCharacters] = React.useState([
+    {
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
-  );
+  <div className="container">
+    <Table
+      characterData={characters}
+      removeCharacter={removeOneCharacter}
+    />
+  </div>
+);
 }
+
 export default MyApp;
