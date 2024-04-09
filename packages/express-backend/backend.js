0a441f83542e60079782a6b6766a6f23c3ffb865
joieng1 <johncieng1@gmail.com>
2024-04-09 16:36:29 -0700

    add post route and readded get all users route

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -46,6 +46,23 @@ app.listen(port, () => {
   );
 });
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
@@ -57,4 +74,16 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  console.log("RAN");
+  res.send();
 });
