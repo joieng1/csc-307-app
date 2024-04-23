56154b58a6d021a653e5b7f7c12d1d468d19a98c
joieng1 <johncieng1@gmail.com>
2024-04-23 16:00:44 -0700

    implement mongoose functions into backend routes

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,40 +1,11 @@
 // backend.js
 import express from "express";
+import userServices from "./models/user-services.js";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 const port = 8000;
-
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor",
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer",
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor",
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress",
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender",
-    },
-  ],
-};
 
 app.use(express.json());
 
@@ -46,36 +17,17 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
 
-app.get("/users", (req, res) => {
+app.get("/users", async (req, res) => {
   const name = req.query.name;
   const job = req.query.job;
-  if (name != undefined) {
-    // if name and job is inputted
-    if (job != undefined) {
-      let result = getAllUsersFilter(name, job);
-      result = { users_list: result };
-      res.send(result);
-      return;
-    }
-    //filter by name only
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-  } else {
-    res.send(users);
-  }
+  const result = await userServices.getUsers(name,job);
+  res.send(result);
 });
 
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-app.get("/users/:id", (req, res) => {
+app.get("/users/:id", async (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
+  let result = await userServices.findUserById(id);
   if (result === undefined) {
     res.status(404).send("Resource not found.");
   } else {
@@ -83,26 +35,17 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const userToAdd = req.body;
-  userToAdd["id"] = Date.now().toString();
-  addUser(userToAdd);
-  res.status(201).send(users);
-});
-
-const deleteUserById = (id) => {
-  const index = users["users_list"].findIndex((user) => user["id"] === id);
-  if(index !== -1) {
-    users["users_list"].splice(index, 1);
-    return true;
+  const result = await userServices.addUser(userToAdd);
+  if (result) {
+  res.status(201).send(result);
   }
-  return false;
-};
+  else {
+    res.send("Failed to post");
+  }
+});
 
 app.delete("/users/:id", (req, res) => {
   const userId = req.params["id"];
@@ -113,11 +56,5 @@ app.delete("/users/:id", (req, res) => {
   else {
     res.status(404).send();
   }
-  
 });
 
-const getAllUsersFilter = (name, job) => {
-  return users["users_list"]
-    .filter((user) => user["name"] === name)
-    .filter((user) => user["job"] === job);
-};
